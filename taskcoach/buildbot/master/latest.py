c136da26c8db4cbe1003addb262af95bba5b6029
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-04 12:08:27 +0100

    Update latest.py

--- taskcoach/buildbot/master/latest.py
+++ taskcoach/buildbot/master/latest.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 
+# 3 lignes de futurize:
+from __future__ import print_function
+from past.builtins import cmp
+from builtins import map
 import os, re, sys
 
 def findLatest(path, valid):
@@ -12,7 +16,9 @@ def findLatest(path, valid):
             mt = rx.search(name)
             if mt:
                 ls = results.get((mt.group(1), mt.group(3)), [])
-                ls.append(map(int, mt.group(2).split('.')))
+                # ls.append(map(int, mt.group(2).split('.')))
+                # futurize:
+                ls.append(list(map(int, mt.group(2).split('.'))))
                 results[(mt.group(1), mt.group(3))] = ls
 
     packages = []
@@ -39,9 +45,15 @@ def listPath(path):
 
     changelog = os.path.join(path, 'changelog_content')
     if os.path.exists(changelog):
-        print '<tr><td colspan="2"><pre>'
-        print file(changelog, 'rb').read()
-        print '</td></tr></pre>'
+        # print '<tr><td colspan="2"><pre>'
+        # print file(changelog, 'rb').read()
+        # print '</td></tr></pre>'
+        # futurize :
+        print('<tr><td colspan="2"><pre>')
+        # print(file(changelog, 'rb').read())
+        # et moi :
+        print(open(changelog, 'rb').read())
+        print('</td></tr></pre>')
 
     print '<tr><th colspan="2"><h2>Sources</h2></th></tr>'
 
@@ -59,7 +71,9 @@ def listPath(path):
     print '<tr><th colspan="2"><h2>Binaries</h2></th></tr>'
 
     for pkgname in findLatest(path, lambda x: not isSource(x)):
-        print '<tr>'
+        # print '<tr>'
+        # futurize :
+        print('<tr>')
         img = 'binary.png'
         if pkgname.endswith('.dmg'):
             img = 'mac.png'
@@ -67,28 +81,38 @@ def listPath(path):
             img = 'windows.png'
         elif pkgname.endswith('.rpm') or pkgname.endswith('.deb'):
             img = 'linux.png'
-        print '<td><img src="%s" /></td>' % img
-        print '<td>'
+        # print '<td><img src="%s" /></td>' % img
+        # print '<td>'
+        # futurize: 
+        print('<td><img src="%s" /></td>' % img)
+        print('<td>')
         if path == '.' or path == 'all':
-            print '<a href="http://www.fraca7.net/TaskCoach-packages/%s">%s</a>' % (pkgname, pkgname)
+            # print '<a href="http://www.fraca7.net/TaskCoach-packages/%s">%s</a>' % (pkgname, pkgname)
+            # futurize :
+            print('<a href="http://www.fraca7.net/TaskCoach-packages/%s">%s</a>' % (pkgname, pkgname))
         else:
-            print '<a href="http://www.fraca7.net/TaskCoach-packages/%s/%s">%s</a>' % (path, pkgname, pkgname)
-        print '</td>'
-        print '</tr>'
+            # print '<a href="http://www.fraca7.net/TaskCoach-packages/%s/%s">%s</a>' % (path, pkgname, pkgname)
+            print('<a href="http://www.fraca7.net/TaskCoach-packages/%s/%s">%s</a>' % (path, pkgname, pkgname))
+        # print '</td>'
+        # print '</tr>'
+        print('</td>')
+        print('</tr>')
 
-    print '</table>'
-    print '<hr />'
+    # print '</table>'
+    # print '<hr />'
+    print('</table>')
+    print('<hr />')
 
 def main(path):
-    print 'Content-type: text/html'
-    print
+    print('Content-type: text/html')
+    print()
 
-    print '<html><head><title>Latest Task Coach builds</title>'
-    print '<style type="text/css" media="screen">@import "default.css";</style>'
-    print '</head><body><center>'
+    print('<html><head><title>Latest Task Coach builds</title>')
+    print('<style type="text/css" media="screen">@import "default.css";</style>')
+    print('</head><body><center>')
 
     if path == '.' or path == 'all':
-        print '<h1>New developments (from trunk)</h1>'
+        print('<h1>New developments (from trunk)</h1>')
         listPath('.')
 
     if path != '.':
@@ -97,14 +121,14 @@ def main(path):
                 fname = os.path.join(path, name)
                 if os.path.isdir(fname):
                     if name.startswith('Release'):
-                        print '<h1>Bug fixes (from %s)</h1>' % name
+                        print('<h1>Bug fixes (from %s)</h1>' % name)
                     else:
-                        print '<h1>Experimental features (from %s)</h1>' % name
+                        print('<h1>Experimental features (from %s)</h1>' % name)
                     listPath(fname)
 
-    print '<a href="http://www.taskcoach.org/download.html>Back to Task Coach downloads</a>'
+    print('<a href="http://www.taskcoach.org/download.html>Back to Task Coach downloads</a>')
 
-    print '</center></body></html>'
+    print('</center></body></html>')
 
 if __name__ == '__main__':
     if sys.argv[0].endswith('latest_features.py'):
