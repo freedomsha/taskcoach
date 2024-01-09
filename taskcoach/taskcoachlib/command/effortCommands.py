8de5137e4bd5e41eaedff29ee0e858c1334a352a
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-09 21:22:45 +0100

    Update effortCommands.py

--- taskcoach/taskcoachlib/command/effortCommands.py
+++ taskcoach/taskcoachlib/command/effortCommands.py
@@ -16,6 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
+from builtins import zip
 from taskcoachlib.domain import effort, date
 from taskcoachlib.i18n import _
 from . import base
