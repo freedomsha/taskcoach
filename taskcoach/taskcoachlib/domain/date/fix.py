6d52db2aef7c216e2171ba3e5ee25501086e9483
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-10 21:40:47 +0100

    Update fix.py

--- taskcoach/taskcoachlib/domain/date/fix.py
+++ taskcoach/taskcoachlib/domain/date/fix.py
@@ -16,6 +16,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
+from builtins import str
+from builtins import object
 from taskcoachlib import operating_system
 import re
 
