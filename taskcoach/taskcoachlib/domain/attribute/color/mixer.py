3697c22575769dcb849d0bd4915c77d40af7e866
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-10 12:00:56 +0100

    Update mixer.py

--- taskcoach/taskcoachlib/domain/attribute/color/mixer.py
+++ taskcoach/taskcoachlib/domain/attribute/color/mixer.py
@@ -16,6 +16,9 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
+from builtins import range
+from builtins import object
+
 
 class ColorMixer(object):
     @staticmethod    
