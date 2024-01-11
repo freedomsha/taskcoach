b12988d76d137b409267b002128c6771049b769f
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-11 20:56:29 +0100

    Update status.py

--- taskcoach/taskcoachlib/domain/task/status.py
+++ taskcoach/taskcoachlib/domain/task/status.py
@@ -16,6 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
+from builtins import object
 from taskcoachlib.i18n import _
 from taskcoachlib.config import defaults
 
