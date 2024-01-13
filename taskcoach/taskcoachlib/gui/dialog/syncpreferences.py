c8e199d317a0dcd8a5b6845e8158a0e0ecbd75b3
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-13 22:44:19 +0100

    Update syncpreferences.py

--- taskcoach/taskcoachlib/gui/dialog/syncpreferences.py
+++ taskcoach/taskcoachlib/gui/dialog/syncpreferences.py
@@ -18,6 +18,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
+from builtins import str
 import wx
 from taskcoachlib.gui.dialog.preferences import SettingsPageBase
 from taskcoachlib import widgets, operating_system
