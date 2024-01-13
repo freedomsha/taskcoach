2fac9afa75d6e2b75513afd5e7365295336987e5
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-13 18:16:49 +0100

    Update attributesync.py

--- taskcoach/taskcoachlib/gui/dialog/attributesync.py
+++ taskcoach/taskcoachlib/gui/dialog/attributesync.py
@@ -16,6 +16,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
+from builtins import str
+from builtins import object
 from taskcoachlib import patterns
 from taskcoachlib.thirdparty.pubsub import pub
 from taskcoachlib.i18n import _
