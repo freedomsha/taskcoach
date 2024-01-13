8e61e144be16542d8331f6d6f7a5c3f94cc8e2d3
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-13 17:47:53 +0100

    Update iocontroller.py

--- taskcoach/taskcoachlib/gui/iocontroller.py
+++ taskcoach/taskcoachlib/gui/iocontroller.py
@@ -18,6 +18,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
+from builtins import object
 from taskcoachlib import meta, persistence, patterns, operating_system
 from taskcoachlib.i18n import _
 from taskcoachlib.thirdparty import lockfile
