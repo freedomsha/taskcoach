0d6cbe957ef92c0886aaa0418090f43369ab19a6
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-13 17:54:19 +0100

    Update mainwindow.py

--- taskcoach/taskcoachlib/gui/mainwindow.py
+++ taskcoach/taskcoachlib/gui/mainwindow.py
@@ -18,6 +18,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
+from builtins import str
 from taskcoachlib import application, meta, widgets, \
     operating_system # pylint: disable=W0622
 from taskcoachlib.gui import viewer, toolbar, uicommand, remindercontroller, \
