22f5d56d2fc9ca3a8f82f90ac5e31768781b4264
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-13 22:26:15 +0100

    Update export.py

--- taskcoach/taskcoachlib/gui/dialog/export.py
+++ taskcoach/taskcoachlib/gui/dialog/export.py
@@ -16,6 +16,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
+from builtins import str
+from builtins import range
 import wx
 from taskcoachlib.i18n import _
 from wx.lib import sized_controls
