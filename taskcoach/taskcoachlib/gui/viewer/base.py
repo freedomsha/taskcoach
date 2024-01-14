3d07e28680c55149c0a8edacb5c17826d268e08b
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-14 22:26:06 +0100

    Update base.py

--- taskcoach/taskcoachlib/gui/viewer/base.py
+++ taskcoach/taskcoachlib/gui/viewer/base.py
@@ -20,6 +20,10 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
+from __future__ import print_function
+
+from builtins import str
+from builtins import object
 import wx
 from taskcoachlib import patterns, widgets, command, render
 from taskcoachlib.i18n import _
