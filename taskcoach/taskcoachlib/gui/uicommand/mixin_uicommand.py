153d08f8c968cbdae9cb9ab8ca9e96d3af94ec6c
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-14 21:18:11 +0100

    Update mixin_uicommand.py

--- taskcoach/taskcoachlib/gui/uicommand/mixin_uicommand.py
+++ taskcoach/taskcoachlib/gui/uicommand/mixin_uicommand.py
@@ -17,7 +17,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-from taskcoachlib.domain import task, note, category, effort, attachment
+from builtins import object
+from ....taskcoachlib.domain import task, note, category, effort, attachment
 import wx
 
 
