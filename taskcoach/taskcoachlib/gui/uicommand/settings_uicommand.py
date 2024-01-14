e80711767ad9a49baec58a090ecda316493d190c
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-14 21:20:08 +0100

    Update settings_uicommand.py

--- taskcoach/taskcoachlib/gui/uicommand/settings_uicommand.py
+++ taskcoach/taskcoachlib/gui/uicommand/settings_uicommand.py
@@ -17,6 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
+from builtins import str
 from . import base_uicommand
 import wx
 
