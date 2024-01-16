c1bcd5347ca716fff335d3cfb91ff97e0ec2ebbb
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-16 21:42:57 +0100

    Update developer_message.py

--- taskcoach/taskcoachlib/gui/dialog/developer_message.py
+++ taskcoach/taskcoachlib/gui/dialog/developer_message.py
@@ -18,8 +18,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import wx
 from wx.lib import hyperlink
-from taskcoachlib import meta
-from taskcoachlib.i18n import _
+from ... import meta
+from ...i18n import _
 from wx.lib import sized_controls
 
 
