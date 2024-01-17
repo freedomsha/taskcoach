370f35f7f94ebd09c880b215229a1eb8cfb1a12c
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-17 20:52:41 +0100

    Update debug.py

--- taskcoach/taskcoachlib/meta/debug.py
+++ taskcoach/taskcoachlib/meta/debug.py
@@ -16,6 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
+from builtins import str
 import sys
 
 def log_call(traceback_depth):
