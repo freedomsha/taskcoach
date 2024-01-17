91440b218bd0bf931958b23d14c3209f7773383f
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-17 21:09:32 +0100

    Update __init__.py

--- taskcoach/taskcoachlib/notify/__init__.py
+++ taskcoach/taskcoachlib/notify/__init__.py
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-from taskcoachlib import operating_system
+from .. import operating_system
 
 
 if operating_system.isWindows():
