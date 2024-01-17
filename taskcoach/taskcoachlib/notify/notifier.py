15ed971aa5685dbc32a64f6b969a5d467d74cb9f
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-17 21:10:38 +0100

    Update notifier.py

--- taskcoach/taskcoachlib/notify/notifier.py
+++ taskcoach/taskcoachlib/notify/notifier.py
@@ -16,7 +16,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-from taskcoachlib import operating_system
+from builtins import object
+from .. import operating_system
 
 
 class AbstractNotifier(object):
