94e41068e78bfe6d940aa009cb59e57706f5f1cb
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-20 15:41:26 +0100

    Update autosaver.py

--- taskcoach/taskcoachlib/persistence/autosaver.py
+++ taskcoach/taskcoachlib/persistence/autosaver.py
@@ -16,7 +16,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-from taskcoachlib.thirdparty.pubsub import pub
+from builtins import object
+from ...thirdparty.pubsub import pub
 import wx
 
 
