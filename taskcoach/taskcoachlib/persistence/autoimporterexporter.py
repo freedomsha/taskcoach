dca30ac6aa1e250cee0ad48939237f6ea1de21c6
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-20 15:40:16 +0100

    Update autoimporterexporter.py

--- taskcoach/taskcoachlib/persistence/autoimporterexporter.py
+++ taskcoach/taskcoachlib/persistence/autoimporterexporter.py
@@ -16,8 +16,10 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-import codecs, os
-from taskcoachlib.thirdparty.pubsub import pub
+from builtins import object
+import codecs
+import os
+from ...thirdparty.pubsub import pub
 from . import todotxt
 
 
