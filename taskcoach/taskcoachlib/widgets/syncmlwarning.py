1e7bf2225623b3250befdb1db83a7a8e231380d0
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-21 20:50:17 +0100

    Update syncmlwarning.py

--- taskcoach/taskcoachlib/widgets/syncmlwarning.py
+++ taskcoach/taskcoachlib/widgets/syncmlwarning.py
@@ -1,4 +1,4 @@
-'''
+"""
 Task Coach - Your friendly task manager
 Copyright (C) 2004-2016 Task Coach developers <developers@taskcoach.org>
 
@@ -14,12 +14,12 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-'''
+"""
 
 
 import wx
 
-from taskcoachlib.i18n import _
+from ..i18n import _
 
 class SyncMLWarningDialog(wx.Dialog):
     def __init__(self, parent):
@@ -38,7 +38,7 @@ class SyncMLWarningDialog(wx.Dialog):
         sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.Add(textWidget, 0, wx.ALL, 10)
         sizer.Add(self.checkbox, 0, wx.ALL, 3)
-        sizer.Add(button, 0, wx.ALL|wx.ALIGN_CENTRE, 3)
+        sizer.Add(button, 0, wx.ALL | wx.ALIGN_CENTRE, 3)
 
         self.SetSizer(sizer)
 
