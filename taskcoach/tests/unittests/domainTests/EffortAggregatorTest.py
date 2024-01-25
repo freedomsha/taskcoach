f098fb661d135fbed081173e8318537bd9fd34b4
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-25 21:41:49 +0100

    Update EffortAggregatorTest.py

--- taskcoach/tests/unittests/domainTests/EffortAggregatorTest.py
+++ taskcoach/tests/unittests/domainTests/EffortAggregatorTest.py
@@ -16,8 +16,9 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-from taskcoachlib import patterns, config
-from taskcoachlib.domain import task, effort, date
+from builtins import object
+from ....taskcoachlib import patterns, config
+from ....taskcoachlib.domain import task, effort, date
 import test
 
 
