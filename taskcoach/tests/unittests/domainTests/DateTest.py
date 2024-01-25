7a731833fc77507e71b46d3f74d0631b6f2c717e
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-25 21:37:18 +0100

    Update DateTest.py

--- taskcoach/tests/unittests/domainTests/DateTest.py
+++ taskcoach/tests/unittests/domainTests/DateTest.py
@@ -16,7 +16,10 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-import test, time, datetime
+from builtins import str
+import test
+import time
+import datetime
 from taskcoachlib.domain import date
 
 
