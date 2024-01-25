0d305c861c61010e0e33a0360c034f8ae067cc9b
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-25 21:48:39 +0100

    Update RecurrenceTest.py

--- taskcoach/tests/unittests/domainTests/RecurrenceTest.py
+++ taskcoach/tests/unittests/domainTests/RecurrenceTest.py
@@ -16,7 +16,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-from taskcoachlib.domain import date
+from builtins import object
+from ....taskcoachlib.domain import date
 import test
 
 
