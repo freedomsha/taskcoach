16426ba46aa7b5f0de8693412d93c06565863c69
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-25 21:50:53 +0100

    Update TaskRelationsTest.py

--- taskcoach/tests/unittests/domainTests/TaskRelationsTest.py
+++ taskcoach/tests/unittests/domainTests/TaskRelationsTest.py
@@ -16,8 +16,10 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-from taskcoachlib import config
-from taskcoachlib.domain import task, date, effort
+from builtins import str
+from builtins import object
+from ....taskcoachlib import config
+from ....taskcoachlib.domain import task, date, effort
 import test
 
  
