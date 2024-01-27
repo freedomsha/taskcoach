6bb093826c93d54056be32f1d64af6730e0d61bb
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-27 18:08:37 +0100

    Update CategoryEditorTest.py

--- taskcoach/tests/unittests/guiTests/CategoryEditorTest.py
+++ taskcoach/tests/unittests/guiTests/CategoryEditorTest.py
@@ -1,4 +1,4 @@
-'''
+"""
 Task Coach - Your friendly task manager
 Copyright (C) 2004-2016 Task Coach developers <developers@taskcoach.org>
 
@@ -14,16 +14,17 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-'''
+"""
 
+from builtins import object
 import wx
 import test
-from taskcoachlib import gui, config, persistence, operating_system
-from taskcoachlib.domain import category, attachment
+from ....taskcoachlib import gui, config, persistence, operating_system
+from ....taskcoachlib.domain import category, attachment
 
 
 class DummyEvent(object):
-    def Skip(self): # pragma: no cover
+    def Skip(self):  # pragma: no cover
         pass
     
 
@@ -35,14 +36,14 @@ class CategoryEditorTest(test.wxTestCase):
         self.categories = self.taskFile.categories()
         self.categories.extend(self.createCategories())
         self.editor = gui.dialog.editor.CategoryEditor(self.frame, 
-            list(self.categories), self.settings, self.categories, 
-            self.taskFile)
+                                                       list(self.categories), self.settings, self.categories, 
+                                                       self.taskFile)
 
     def tearDown(self):
         # CategoryEditor uses CallAfter for setting the focus, make sure those 
         # calls are dealt with, otherwise they'll turn up in other tests
         if operating_system.isGTK():
-            wx.Yield() # pragma: no cover 
+            wx.Yield()  # pragma: no cover 
         super(CategoryEditorTest, self).tearDown()
         self.taskFile.close()
         self.taskFile.stop()
@@ -60,16 +61,16 @@ class CategoryEditorTest(test.wxTestCase):
         page = self.editor._interior[0]
         page._subjectEntry.SetFocus()
         page._subjectEntry.SetValue(newSubject)
-        if operating_system.isGTK(): # pragma: no cover 
+        if operating_system.isGTK():  # pragma: no cover 
             page._subjectSync.onAttributeEdited(DummyEvent())
-        else: # pragma: no cover 
+        else:  # pragma: no cover 
             page._descriptionEntry.SetFocus()
 
     def setDescription(self, newDescription):
         page = self.editor._interior[0]
         page._descriptionEntry.SetFocus()
         page._descriptionEntry.SetValue(newDescription)
-        if operating_system.isGTK(): # pragma: no cover 
+        if operating_system.isGTK():  # pragma: no cover 
             page._descriptionSync.onAttributeEdited(DummyEvent())
         else:  # pragma: no cover
             page._subjectEntry.SetFocus()
