319edd69b0bda5b16e9b9d273a444f7e682b844d
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-27 21:24:34 +0100

    Update MainWindowTest.py

--- taskcoach/tests/unittests/guiTests/MainWindowTest.py
+++ taskcoach/tests/unittests/guiTests/MainWindowTest.py
@@ -1,4 +1,4 @@
-'''
+"""
 Task Coach - Your friendly task manager
 Copyright (C) 2004-2016 Task Coach developers <developers@taskcoach.org>
 
@@ -14,11 +14,13 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-'''
+"""
 
-import wx, test
-from taskcoachlib import gui, config, persistence, meta, operating_system
-from taskcoachlib.domain import task
+from builtins import object
+import wx
+import test
+from ....taskcoachlib import gui, config, persistence, meta, operating_system
+from ....taskcoachlib.domain import task
 
 
 class MockViewer(wx.Frame):
@@ -44,11 +46,11 @@ class MainWindowUnderTest(gui.MainWindow):
     
 
 class DummyIOController(object):
-    def needSave(self, *args, **kwargs): # pylint: disable=W0613
-        return False # pragma: no cover
+    def needSave(self, *args, **kwargs):  # pylint: disable=W0613
+        return False  # pragma: no cover
 
     def changedOnDisk(self):
-        return False # pragme: no cover
+        return False  # pragme: no cover
 
 
 class MainWindowTestCase(test.wxTestCase):
@@ -59,14 +61,14 @@ class MainWindowTestCase(test.wxTestCase):
         task.Task.settings = self.settings
         self.taskFile = persistence.TaskFile()
         self.mainwindow = MainWindowUnderTest(DummyIOController(),
-            self.taskFile, self.settings)
+                                              self.taskFile, self.settings)
             
     def setSettings(self):
         pass
 
     def tearDown(self):
         if operating_system.isMac():
-            self.mainwindow.OnQuit() # Stop power monitoring thread
+            self.mainwindow.OnQuit()  # Stop power monitoring thread
         # Also stop idle time thread
         self.mainwindow._idleController.stop()
         self.mainwindow.Destroy()
@@ -91,8 +93,8 @@ class MainWindowTest(MainWindowTestCase):
         
     def testTitle_AfterFilenameChange(self):
         self.taskFile.setFilename('New filename')
-        self.assertEqual('%s - %s'%(meta.name, self.taskFile.filename()), 
-            self.mainwindow.GetTitle())
+        self.assertEqual('%s - %s' % (meta.name, self.taskFile.filename()), 
+                         self.mainwindow.GetTitle())
 
     def testTitle_AfterChange(self):
         self.taskFile.setFilename('New filename')
@@ -114,7 +116,7 @@ class MainWindowMaximizeTestCase(MainWindowTestCase):
     def setUp(self):
         super(MainWindowMaximizeTestCase, self).setUp()
         if not operating_system.isMac():
-            self.mainwindow.Show() # Or IsMaximized() returns always False...
+            self.mainwindow.Show()  # Or IsMaximized() returns always False...
         
     def setSettings(self):
         self.settings.setboolean('window', 'maximized', self.maximized)
@@ -127,7 +129,7 @@ class MainWindowNotMaximizedTest(MainWindowMaximizeTestCase):
         self.failIf(self.mainwindow.IsMaximized())
 
     @test.skipOnPlatform('__WXGTK__')
-    def testMaximize(self): # pragma: no cover
+    def testMaximize(self):  # pragma: no cover
         # Skipping this test under wxGTK. I don't know how it managed
         # to pass before but according to
         # http://trac.wxwidgets.org/ticket/9167 and to my own tests,
@@ -143,14 +145,14 @@ class MainWindowMaximizedTest(MainWindowMaximizeTestCase):
 
     @test.skipOnPlatform('__WXMAC__')
     def testCreate(self):
-        self.failUnless(self.mainwindow.IsMaximized()) # pragma: no cover
+        self.failUnless(self.mainwindow.IsMaximized())  # pragma: no cover
 
 
 class MainWindowIconizedTest(MainWindowTestCase):
     def setUp(self):
         super(MainWindowIconizedTest, self).setUp()        
         if operating_system.isGTK():
-            wx.SafeYield() # pragma: no cover
+            wx.SafeYield()  # pragma: no cover
             
     def setSettings(self):
         self.settings.set('window', 'starticonized', 'Always')
@@ -158,12 +160,12 @@ class MainWindowIconizedTest(MainWindowTestCase):
     def expectedHeight(self):
         height = 500
         if operating_system.isMac():
-            height += 18 # pragma: no cover
+            height += 18  # pragma: no cover
         return height
     
-    @test.skipOnPlatform('__WXGTK__') # Test fails on Fedora, don't know why nor how to fix it    
+    @test.skipOnPlatform('__WXGTK__')  # Test fails on Fedora, don't know why nor how to fix it    
     def testIsIconized(self):
-        self.failUnless(self.mainwindow.IsIconized()) # pragma: no cover
+        self.failUnless(self.mainwindow.IsIconized())  # pragma: no cover
                         
     def testWindowSize(self):
         self.assertEqual((900, self.expectedHeight()), 
@@ -173,9 +175,8 @@ class MainWindowIconizedTest(MainWindowTestCase):
         event = wx.SizeEvent((100, 20))
         process = self.mainwindow.ProcessEvent
         if operating_system.isWindows():
-            process(event) # pragma: no cover
+            process(event)  # pragma: no cover
         else:
-            wx.CallAfter(process, event) # pragma: no cover
+            wx.CallAfter(process, event)  # pragma: no cover
         self.assertEqual((900, self.expectedHeight()),
                          eval(self.settings.get('window', 'size')))
-
