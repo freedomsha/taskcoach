3f2c10b5e2931e64cdd3c6b434c8e11889de997a
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-27 21:41:30 +0100

    Update PreferencesTest.py

--- taskcoach/tests/unittests/guiTests/PreferencesTest.py
+++ taskcoach/tests/unittests/guiTests/PreferencesTest.py
@@ -1,4 +1,4 @@
-'''
+"""
 Task Coach - Your friendly task manager
 Copyright (C) 2004-2016 Task Coach developers <developers@taskcoach.org>
 
@@ -14,10 +14,10 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-'''
+"""
 
 import test
-from taskcoachlib import gui, config, persistence
+from ....taskcoachlib import gui, config, persistence
 
 
 class PreferencesTest(test.wxTestCase):
@@ -25,7 +25,7 @@ class PreferencesTest(test.wxTestCase):
         super(PreferencesTest, self).setUp()
         self.settings = config.Settings(load=False)
         self.preferences = gui.Preferences(parent=self.frame, title='Test',
-            settings=self.settings)
+                                           settings=self.settings)
         self.originalColor = self.settings.get('fgcolor', 'activetasks')
         self.newColor = (1, 2, 29)
         
@@ -40,7 +40,7 @@ class PreferencesTest(test.wxTestCase):
         self.preferences[5]._colorSettings[4][2].SetColour(self.newColor)
         self.preferences.ok()
         self.assertEqual(self.newColor, 
-            eval(self.settings.get('fgcolor', 'activetasks'))[:3])
+                         eval(self.settings.get('fgcolor', 'activetasks'))[:3])
         
 
 class SyncMLPreferencesTest(test.TestCase):
@@ -48,8 +48,8 @@ class SyncMLPreferencesTest(test.TestCase):
         taskFile = persistence.TaskFile()
         try:
             gui.dialog.syncpreferences.SyncMLPreferences(parent=None,
-                  iocontroller=gui.IOController(taskFile, None, None), 
-                  title='Edit SyncML preferences')
+                                                         iocontroller=gui.IOController(taskFile, None, None), 
+                                                         title='Edit SyncML preferences')
         finally:
             taskFile.close()
             taskFile.stop()
