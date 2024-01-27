0e2502a7b027559d256bced9220f461878b5ab63
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-27 18:21:16 +0100

    Update EffortEditorTest.py

--- taskcoach/tests/unittests/guiTests/EffortEditorTest.py
+++ taskcoach/tests/unittests/guiTests/EffortEditorTest.py
@@ -1,4 +1,4 @@
-'''
+"""
 Task Coach - Your friendly task manager
 Copyright (C) 2004-2016 Task Coach developers <developers@taskcoach.org>
 
@@ -14,12 +14,12 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-'''
+"""
 
 import test
-from taskcoachlib import gui, config, persistence
-from taskcoachlib.domain import task, effort, date
-from unittests import dummy
+from ....taskcoachlib import gui, config, persistence
+from ....taskcoachlib.domain import task, effort, date
+from ...unittests import dummy
 
 
 class EditorUnderTest(gui.dialog.editor.EffortEditor):        
@@ -27,7 +27,7 @@ class EditorUnderTest(gui.dialog.editor.EffortEditor):
         super(EditorUnderTest, self).__init__(*args, **kwargs)
         self.editorClosed = False
                 
-    def onClose(self, event): # pragma: no cover
+    def onClose(self, event):  # pragma: no cover
         self.editorClosed = True
         super(EditorUnderTest, self).onClose(event)
         
@@ -45,8 +45,8 @@ class EffortEditorTest(test.wxTestCase):
         self.task2 = task.Task('task2')
         self.taskFile.tasks().extend([self.task, self.task2])
         self.editor = EditorUnderTest(self.frame, 
-            list(self.effortList), self.settings, self.taskFile.efforts(), 
-            self.taskFile, raiseDialog=False)
+                                      list(self.effortList), self.settings, self.taskFile.efforts(), 
+                                      self.taskFile, raiseDialog=False)
 
     def tearDown(self):
         super(EffortEditorTest, self).tearDown()
@@ -55,17 +55,17 @@ class EffortEditorTest(test.wxTestCase):
 
     def createEditor(self):
         return EditorUnderTest(self.frame,
-            list(self.taskFile.efforts()), self.settings, 
-            self.taskFile.efforts(), self.taskFile)        
+                               list(self.taskFile.efforts()), self.settings, 
+                               self.taskFile.efforts(), self.taskFile)        
 
     # pylint: disable=W0201,W0212
         
     def testCreate(self):
         self.assertEqual(self.task, self.editor._interior._taskEntry.GetValue())
         self.assertEqual(self.effort.getStart().date(), 
-            self.editor._interior._startDateTimeEntry.GetValue().date())
+                         self.editor._interior._startDateTimeEntry.GetValue().date())
         self.assertEqual(self.effort.task(), 
-            self.editor._interior._taskEntry.GetValue())    
+                         self.editor._interior._taskEntry.GetValue())    
         
     def testInvalidEffort(self):    
         self.editor._interior._stopDateTimeEntry.SetValue(date.DateTime(1900, 1, 1))
