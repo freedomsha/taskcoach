87bc3e6fe343413792f23e2e64f849fdeea88e7c
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-28 09:38:59 +0100

    Update TemplatesDialogTest.py

--- taskcoach/tests/unittests/guiTests/TemplatesDialogTest.py
+++ taskcoach/tests/unittests/guiTests/TemplatesDialogTest.py
@@ -1,4 +1,4 @@
-'''
+"""
 Task Coach - Your friendly task manager
 Copyright (C) 2004-2016 Task Coach developers <developers@taskcoach.org>
 
@@ -14,10 +14,12 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-'''
+"""
 
-import test, os, shutil
-from taskcoachlib import gui, config
+import test
+import os
+import shutil
+from ....taskcoachlib import gui, config
 
  
 class TemplatesDialogTestCase(test.wxTestCase):
@@ -42,8 +44,8 @@ class TemplatesDialogTestCase(test.wxTestCase):
     def safelyRemove(self, path):
         try:
             shutil.rmtree(path)
-        except OSError: # pragma: no cover
+        except OSError:  # pragma: no cover
             pass
 
     def testTwoDefaultTemplates(self):
-        self.assertEqual(0, len(self.editor._templates.tasks())) # pylint: disable=W0212
+        self.assertEqual(0, len(self.editor._templates.tasks()))  # pylint: disable=W0212
