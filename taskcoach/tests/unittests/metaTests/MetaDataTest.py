04dae96e04b1bb2903521379f5ad35c933c9fd49
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-28 10:44:59 +0100

    Update MetaDataTest.py

--- taskcoach/tests/unittests/metaTests/MetaDataTest.py
+++ taskcoach/tests/unittests/metaTests/MetaDataTest.py
@@ -1,4 +1,4 @@
-'''
+"""
 Task Coach - Your friendly task manager
 Copyright (C) 2004-2016 Task Coach developers <developers@taskcoach.org>
 
@@ -14,10 +14,12 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-'''
+"""
 
-import test, datetime
-from taskcoachlib import meta
+from builtins import str
+import test
+import datetime
+from ....taskcoachlib import meta
 
 
 class VersionNumberTest(test.TestCase):
