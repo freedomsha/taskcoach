37643c3e157802ae5f2d4389f7de2666dfdd2dbd
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-29 20:45:06 +0100

    Update PubSubTest.py

--- taskcoach/tests/unittests/thirdPartySoftwareTests/PubSubTest.py
+++ taskcoach/tests/unittests/thirdPartySoftwareTests/PubSubTest.py
@@ -1,4 +1,4 @@
-'''
+"""
 Task Coach - Your friendly task manager
 Copyright (C) 2004-2016 Task Coach developers <developers@taskcoach.org>
 
@@ -14,10 +14,10 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-'''
+"""
 
-from taskcoachlib.thirdparty.pubsub import pub
-import test
+from ....taskcoachlib.thirdparty.pubsub import pub
+from ... import test
 
 
 class PubSubTest(test.TestCase):
@@ -32,4 +32,3 @@ class PubSubTest(test.TestCase):
         pub.sendMessage('TestTopic')
         self.failUnless(self.calledTestTopic)
         
- 
