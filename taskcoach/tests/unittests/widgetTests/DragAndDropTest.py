5601fcab00e06c3c0314f579ecf893117f034969
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-29 20:58:24 +0100

    Update DragAndDropTest.py

--- taskcoach/tests/unittests/widgetTests/DragAndDropTest.py
+++ taskcoach/tests/unittests/widgetTests/DragAndDropTest.py
@@ -16,8 +16,10 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-import test, wx
-from taskcoachlib.widgets import treectrl
+from builtins import object
+from ... import test
+import wx
+from ....taskcoachlib.widgets import treectrl
 
 
 class DummyEvent(object):
