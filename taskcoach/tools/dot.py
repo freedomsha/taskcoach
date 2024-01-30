0845a9bd5e576409ad6a291a22040c694af514d6
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-01-30 21:10:03 +0100

    Update dot.py

--- taskcoach/tools/dot.py
+++ taskcoach/tools/dot.py
@@ -19,7 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from __future__ import print_function
 import os, re, glob, sys
 
-
 packages = []
 modules = []
 classdef = re.compile('class ([A-Za-z]+)\(([^)]+)\)', re.MULTILINE)
@@ -28,7 +27,8 @@ classdef = re.compile('class ([A-Za-z]+)\(([^)]+)\)', re.MULTILINE)
 def stripmodule(classname):
     return classname.split('.')[-1]
 
-print 'digraph G {\nrankdir="LR"'
+# print 'digraph G {\nrankdir="LR"'
+print('digraph G {\nrankdir="LR"')
 
 for filename in glob.glob(os.path.join(sys.argv[1], '*.py')):
     contents = file(filename).read()
@@ -37,18 +37,18 @@ for filename in glob.glob(os.path.join(sys.argv[1], '*.py')):
         continue
 
     module = os.path.basename(filename)[:-len('.py')]
-    print 'subgraph cluster%s {'%module
-    print 'label=%s'%module
-    print ' '.join([classes[0] for classes in matches])
-    print '}\n'
+    print('subgraph cluster%s {' % module)
+    print('label=%s' % module)
+    print(' '.join([classes[0] for classes in matches]))
+    print('}\n')
     for match in matches:
         class_ = stripmodule(match[0])
         superclasses = re.sub('\s', '', match[1]).split(',')
         for superclass in superclasses:
             if superclass == 'object':
                 continue
-            print '%s->%s'%(stripmodule(superclass), class_)
-    print
+            print('%s->%s' % (stripmodule(superclass), class_))
+    print()
 
-print '}'
+print('}')
 
