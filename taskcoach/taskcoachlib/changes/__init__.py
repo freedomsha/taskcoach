c3dd30e9fcb1fbcf5ef04fc84f7b46c8a71024ff
freedomsha <154604793+freedomsha@users.noreply.github.com>
2024-03-25 20:17:42 +0100

    Update __init__.py

--- taskcoach/taskcoachlib/changes/__init__.py
+++ taskcoach/taskcoachlib/changes/__init__.py
@@ -1,4 +1,4 @@
-'''
+"""
 Task Coach - Your friendly task manager
 Copyright (C) 2011 Task Coach developers <developers@taskcoach.org>
 
@@ -14,7 +14,11 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-'''
+"""
 
-from .monitor import ChangeMonitor
-from .sync import ChangeSynchronizer
+try:
+  from .monitor import ChangeMonitor
+  from .sync import ChangeSynchronizer
+except ImportError:
+  from taskcoachlib.changes.monitor import ChangeMonitor
+  from taskcoachlib.changes.sync import ChangeSynchronizer
