ceefe572dc05ac88d31873128c159df1937314dd
sylvain <szydlowskis@aol.com>
2024-10-04 16:22:42 +0200

    improvment

--- taskcoach/tests/unittests/AppTest.py
+++ taskcoach/tests/unittests/AppTest.py
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import test
+from .. import test
 import wx
 from taskcoachlib import meta, application, config
 
