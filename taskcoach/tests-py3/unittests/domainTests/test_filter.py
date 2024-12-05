70027eb0326244a8e61a7d3c5f0e3b21b8b3fb6f
sylvain <szydlowskis@aol.com>
2024-12-05 17:33:00 +0100

    test_filter ok

--- taskcoach/tests-py3/unittests/domainTests/test_filter.py
+++ taskcoach/tests-py3/unittests/domainTests/test_filter.py
@@ -1,4 +1,4 @@
-'''
+"""
 Task Coach - Your friendly task manager
 Copyright (C) 2004-2016 Task Coach developers <developers@taskcoach.org>
 
@@ -14,43 +14,43 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-'''
+"""
 
-from builtins import object
+# from builtins import object
 import weakref
 
-import tctest
+from ... import tctest
 from taskcoachlib import patterns, config
 from taskcoachlib.domain import task, base
 
 
 class FilterUnderTest(base.Filter):
     def filterItems(self, items):
-        return [item for item in items if item > 'b']
+        return [item for item in items if item > "b"]
 
 
 class FilterTestsMixin(object):
     def setUp(self):
         super().setUp()
-        self.observable = self.collectionClass(['a', 'b', 'c', 'd'])
+        self.observable = self.collectionClass(["a", "b", "c", "d"])
         self.filter = FilterUnderTest(self.observable)
 
     def testLength(self):
         self.assertEqual(2, len(self.filter))
 
     def testContents(self):
-        self.assertTrue('c' in self.filter and 'd' in self.filter)
+        self.assertTrue("c" in self.filter and "d" in self.filter)
 
     def testRemoveItem(self):
-        self.filter.remove('c')
+        self.filter.remove("c")
         self.assertEqual(1, len(self.filter))
-        self.assertTrue('d' in self.filter)
-        self.assertEqual({'a', 'b', 'd'}, set(self.observable))
+        self.assertTrue("d" in self.filter)
+        self.assertEqual({"a", "b", "d"}, set(self.observable))
 
     def testNotification(self):
-        self.observable.append('e')
+        self.observable.append("e")
         self.assertEqual(3, len(self.filter))
-        self.assertTrue('e' in self.filter)
+        self.assertTrue("e" in self.filter)
 
 
 class FilterListTest(FilterTestsMixin, tctest.TestCase):
@@ -77,8 +77,9 @@ class DummyItem(str):
 class StackedFilterTest(tctest.TestCase):
     def setUp(self):
         super().setUp()
-        self.list = patterns.ObservableList([DummyItem('a'), DummyItem('b'),
-                                             DummyItem('c'), DummyItem('d')])
+        self.list = patterns.ObservableList(
+            [DummyItem("a"), DummyItem("b"), DummyItem("c"), DummyItem("d")]
+        )
         self.filter1 = DummyFilter(self.list)
         self.filter2 = FilterUnderTest(self.filter1)
 
@@ -106,105 +107,115 @@ class SearchFilterTest(tctest.TestCase):
     def setUp(self):
         super().setUp()
         task.Task.settings = config.Settings(load=False)
-        self.parent = task.Task(subject='*ABC$D', description='Parent description')
-        self.child = task.Task(subject='DEF', description='Child description')
+        self.parent = task.Task(subject="*ABC$D", description="Parent description")
+        self.child = task.Task(subject="DEF", description="Child description")
         self.parent.addChild(self.child)
         self.list = task.TaskList([self.parent, self.child])
         self.filter = base.SearchFilter(self.list)
 
-    def setSearchString(self, searchString, matchCase=False,
-                        includeSubItems=False, searchDescription=False, regularExpression=True):
-        self.filter.setSearchFilter(searchString, matchCase=matchCase,
-                                    includeSubItems=includeSubItems,
-                                    searchDescription=searchDescription,
-                                    regularExpression=regularExpression)
+    def setSearchString(
+        self,
+        searchString,
+        matchCase=False,
+        includeSubItems=False,
+        searchDescription=False,
+        regularExpression=True,
+    ):
+        self.filter.setSearchFilter(
+            searchString,
+            matchCase=matchCase,
+            includeSubItems=includeSubItems,
+            searchDescription=searchDescription,
+            regularExpression=regularExpression,
+        )
 
     def testNoMatch(self):
-        self.setSearchString('XYZ')
+        self.setSearchString("XYZ")
         self.assertEqual(0, len(self.filter))
 
     def testMatch(self):
-        self.setSearchString('AB')
+        self.setSearchString("AB")
         self.assertEqual(1, len(self.filter))
 
     def testMatchIsCaseInSensitiveByDefault(self):
-        self.setSearchString('abc')
+        self.setSearchString("abc")
         self.assertEqual(1, len(self.filter))
 
     def testMatchCaseInsensitive(self):
-        self.setSearchString('abc', True)
+        self.setSearchString("abc", True)
         self.assertEqual(0, len(self.filter))
 
     def testMatchWithRE(self):
-        self.setSearchString('a.c')
+        self.setSearchString("a.c")
         self.assertEqual(1, len(self.filter))
 
     def testMatchWithoutRE(self):
-        self.setSearchString('$D', regularExpression=False)
+        self.setSearchString("$D", regularExpression=False)
         self.assertEqual(1, len(self.filter))
 
     def testMatchWithEmptyString(self):
-        self.setSearchString('')
+        self.setSearchString("")
         self.assertEqual(2, len(self.filter))
 
     def testMatchChildDoesNotSelectParentWhenNotInTreeMode(self):
-        self.setSearchString('DEF')
+        self.setSearchString("DEF")
         self.assertEqual(1, len(self.filter))
 
     def testMatchChildAlsoSelectsParentWhenInTreeMode(self):
         self.filter.setTreeMode(True)
-        self.setSearchString('DEF')
+        self.setSearchString("DEF")
         self.assertEqual(2, len(self.filter))
 
     def testMatchChildDoesNotSelectParentWhenChildNotInList(self):
         self.list.remove(self.child)
-        self.parent.addChild(self.child) # simulate a child that has been filtered
-        self.setSearchString('DEF')
+        self.parent.addChild(self.child)  # simulate a child that has been filtered
+        self.setSearchString("DEF")
         self.assertEqual(0, len(self.filter))
 
     def testAddTask(self):
-        self.setSearchString('XYZ')
-        taskXYZ = task.Task(subject='subject with XYZ')
+        self.setSearchString("XYZ")
+        taskXYZ = task.Task(subject="subject with XYZ")
         self.list.append(taskXYZ)
         self.assertEqual([taskXYZ], list(self.filter))
 
     def testRemoveTask(self):
-        self.setSearchString('DEF')
+        self.setSearchString("DEF")
         self.list.remove(self.child)
         self.assertFalse(self.filter)
 
     def testIncludeSubItems(self):
-        self.setSearchString('ABC', includeSubItems=True)
+        self.setSearchString("ABC", includeSubItems=True)
         self.assertEqual(2, len(self.filter))
 
     def testInvalidRegex(self):
-        self.setSearchString('*')
+        self.setSearchString("*")
         self.assertEqual(1, len(self.filter))
 
     def testInvalidRegex_WhileMatchCase(self):
-        self.setSearchString('*', matchCase=True)
+        self.setSearchString("*", matchCase=True)
         self.assertEqual(1, len(self.filter))
 
     def testSearchDescription(self):
-        self.setSearchString('parent description', searchDescription=True)
+        self.setSearchString("parent description", searchDescription=True)
         self.assertEqual(1, len(self.filter))
 
     def testSearchDescription_TurnedOff(self):
-        self.setSearchString('parent description')
+        self.setSearchString("parent description")
         self.assertEqual(0, len(self.filter))
 
     def testSearchDescriptionWithSubItemsIncluded(self):
-        self.setSearchString('parent description', includeSubItems=True,
-                             searchDescription=True)
+        self.setSearchString(
+            "parent description", includeSubItems=True, searchDescription=True
+        )
         self.assertEqual(2, len(self.filter))
 
     def testSearchDescription_MatchChildDoesNotSelectParentWhenNotInTreeMode(self):
-        self.setSearchString('child description', searchDescription=True)
+        self.setSearchString("child description", searchDescription=True)
         self.assertEqual(1, len(self.filter))
 
     def testSearchDescription_MatchChildAlsoSelectsParentWhenInTreeMode(self):
         self.filter.setTreeMode(True)
-        self.setSearchString('child description', searchDescription=True)
+        self.setSearchString("child description", searchDescription=True)
         self.assertEqual(2, len(self.filter))
 
 
@@ -244,8 +255,7 @@ class SelectedItemsFilterTest(tctest.TestCase):
         self.task = task.Task()
         self.child = task.Task(parent=self.task)
         self.list = task.TaskList([self.task])
-        self.filter = base.SelectedItemsFilter(self.list,
-                                               selectedItems=[self.task])
+        self.filter = base.SelectedItemsFilter(self.list, selectedItems=[self.task])
 
     def testInitialContent(self):
         self.assertEqual([self.task], list(self.filter))
