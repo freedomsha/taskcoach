016327a7cef6f11419dd46dfd0938b5b62c4e868
sylvain <szydlowskis@aol.com>
2024-12-14 18:46:38 +0100

    test_lockfile ok

--- taskcoach/tests-py3/unittests/thirdPartySoftwareTests/test_lockfile.py
+++ taskcoach/tests-py3/unittests/thirdPartySoftwareTests/test_lockfile.py
@@ -16,11 +16,9 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import tempfile
-
 import lockfile
-
-import tctest
+import tempfile
+from ... import tctest
 
 
 class LockFileTest(tctest.TestCase):
