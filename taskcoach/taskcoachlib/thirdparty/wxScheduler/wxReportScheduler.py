f7f618c5554dbc1aaa88fe88f21412d5e75f76a3
sylvain <szydlowskis@aol.com>
2025-01-03 07:55:48 +0100

    imports * -> wxSchedulerPrint

--- taskcoach/taskcoachlib/thirdparty/wxScheduler/wxReportScheduler.py
+++ taskcoach/taskcoachlib/thirdparty/wxScheduler/wxReportScheduler.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from .wxSchedulerPrint import *
+# from .wxSchedulerPrint import *
+from .wxSchedulerPrint import wxSchedulerPrint
 import calendar
 import wx
 
