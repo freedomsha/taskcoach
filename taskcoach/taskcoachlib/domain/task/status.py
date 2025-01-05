991ec42f49b60d53be138cf24c5762d27bb4d26d
sylvain <szydlowskis@aol.com>
2025-01-05 06:57:33 +0100

    Docstrings

--- taskcoach/taskcoachlib/domain/task/status.py
+++ taskcoach/taskcoachlib/domain/task/status.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Task Coach - Your friendly task manager
 Copyright (C) 2004-2016 Task Coach developers <developers@taskcoach.org>
@@ -14,6 +15,33 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Ce module définit la classe `TaskStatus` et plusieurs instances représentant différents statuts de tâche.
+
+La classe `TaskStatus` encapsule des informations sur un statut de tâche spécifique, notamment :
+
+* `statusString` : un court identifiant de chaîne pour le statut.
+* `pluralLabel` : une étiquette lisible par l'homme pour la forme plurielle du statut.
+* `countLabel` : Un format de chaîne pour afficher le nombre de tâches avec ce statut.
+* `hideMenuText` : Le texte à afficher dans le menu pour masquer les tâches avec ce statut.
+* `hideHelpText` : Un texte d'aide expliquant la signification du statut.
+
+Il fournit également des méthodes pour :
+
+* Récupérer l'icône appropriée en fonction du statut et paramètres.
+* Obtenir le texte pour masquer l'état.
+* Comparaison des objets d'état.
+* Hachage des objets d'état à utiliser dans les collections.
+
+Le module définit plusieurs instances de `TaskStatus` classe pour les statuts de tâches courants :
+* `inactif`
+* `tard`
+* `actif`
+* `duesoon`
+* `overdue`
+* `completed`
+
+Chaque instance possède ses propres attributs spécifiques et fournit un moyen pratique de représenter et de travailler avec différents statuts de tâches au sein de l'application.
 """
 
 # from builtins import object
@@ -22,6 +50,16 @@ from taskcoachlib.config import defaults
 
 
 class TaskStatus(object):
+    """
+    Représente un statut spécifique pour une tâche.
+
+    Attributs :
+        statusString (str) : Un identifiant de chaîne courte pour le statut.
+        pluralLabel (str) : Une étiquette lisible par l'homme pour la forme plurielle de le statut.
+        countLabel (str) : Un format de chaîne pour afficher le nombre de tâches avec ce statut.
+        hideMenuText (str) : Le texte à afficher dans le menu pour masquer les tâches avec ce statut. status.
+        hideHelpText (str) : Un texte d'aide expliquant la signification du statut.
+    """
     def __init__(self, statusString, pluralLabel, countLabel, hideMenuText,
                  hideHelpText):
         self.statusString = statusString
@@ -30,15 +68,33 @@ class TaskStatus(object):
         self.hideMenuText = hideMenuText
         self.hideHelpText = hideHelpText
 
-    # This is only used by uicommands so use default if the user configured 'no bitmap', because we
-    # need one for the toolbar...
+    # Ceci n'est utilisé que par les commandes ui, donc utilisez la valeur par défaut si l'utilisateur a configuré 'pas de bitmap', car nous
+    # en avons besoin pour la barre d'outils...
 
     def getBitmap(self, settings):
+        """
+        Récupère l'icône associée au statut.
+
+        Args :
+            settings (object ?) : L'objet des paramètres de l'application.
+
+        Returns :
+            str : Le chemin d'accès au fichier d'icône.
+        """
         if settings.get("icon", "%stasks" % self.statusString):
             return settings.get("icon", "%stasks" % self.statusString)
         return defaults.defaults["icon"]["%stasks" % self.statusString]
 
     def getHideBitmap(self, settings):
+        """
+        Récupère l'icône pour masquer les tâches avec ce statut.
+
+        Args :
+            settings (object ?) : L'objet des paramètres de l'application.
+
+        Renvoie :
+            str : Le chemin d'accès de l'icône à déposer.
+        """
         if settings.get("icon", "%stasks" % self.statusString):
             return "%s+cross_red_icon" % settings.get(
                 "icon", "%stasks" % self.statusString
@@ -49,24 +105,66 @@ class TaskStatus(object):
         )
 
     def __repr__(self):
+        """
+        Renvoie une représentation sous forme de chaîne de l'objet TaskStatus.
+
+        Renvoie :
+            str : Une représentation sous forme de chaîne de l'objet.
+        """
         return "%s(%s)" % (self.__class__.__name__, self.statusString)
 
     def __str__(self):
+        """
+        Renvoie la chaîne d'état.
+
+        Renvoie :
+            str : La chaîne d'état.
+        """
         return self.statusString
 
     def __eq__(self, other):
+        """
+        Vérifie si deux objets TaskStatus sont égaux.
+
+        Args:
+            other (TaskStatus) : l'autre objet TaskStatus à comparer.
+
+        Returns:
+            bool : True si les chaînes d'état sont égal, Faux sinon.
+        """
         return self.statusString == other.statusString
 
 # j'ai ajouter cette fonction:
     def __hash__(self) -> int:
+        """
+        Renvoie le hachage de l'objet TaskStatus.
+
+        Renvoie :
+            int : La valeur de hachage.
+        """
         # Because of __eq__
         # return hash(id(self))
         return hash(self.statusString)
 
     def __neq__(self, other):
+        """
+        Vérifie si deux objets TaskStatus ne sont pas égaux.
+
+        Args:
+            other (TaskStatus) : L'autre objet TaskStatus à comparer.
+
+        Returns:
+            bool : True si les chaînes d'état ne sont pas égaux, Faux sinon.
+        """
         return self.statusString != other.statusString
 
     def __bool__(self):
+        """
+        Renvoie True pour tous les objets TaskStatus.
+
+        Renvoie :
+            bool : toujours vrai.
+        """
         return True
 
 
