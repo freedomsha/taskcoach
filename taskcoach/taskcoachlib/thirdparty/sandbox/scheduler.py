7992c81129ba1f8acc182bbad546ad2b7ae89933
sylvain <szydlowskis@aol.com>
2025-01-05 07:29:47 +0100

    import thread->_thread, use Black,

--- taskcoach/taskcoachlib/thirdparty/sandbox/scheduler.py
+++ taskcoach/taskcoachlib/thirdparty/sandbox/scheduler.py
@@ -4,19 +4,21 @@ Copyright (c) 2003-2005  Gustavo Niemeyer <gustavo@niemeyer.net>
 This module offers extensions to the standard python 2.3+
 datetime module.
 """
+
 __author__ = "Gustavo Niemeyer <gustavo@niemeyer.net>"
 __license__ = "PSF License"
 
 import datetime
-import thread
+import _thread
 import signal
 import time
 
+
 class sched:
 
-    def __init__(self, rrule,
-                 tolerance=None, last=None,
-                 execute=None, args=None, kwargs=None):
+    def __init__(
+        self, rrule, tolerance=None, last=None, execute=None, args=None, kwargs=None
+    ):
         self._rrule = rrule
         if tolerance:
             self._tolerance = datetime.timedelta(seconds=tolerance)
@@ -39,8 +41,11 @@ class sched:
         if not now:
             now = datetime.datetime.now()
         item = self._rrule.before(now, inc=True)
-        if (item is None or item == self._last or
-            (self._tolerance and item+self._tolerance < now)):
+        if (
+            item is None
+            or item == self._last
+            or (self._tolerance and item + self._tolerance < now)
+        ):
             return None
         if not readonly:
             self._last = item
@@ -77,7 +82,7 @@ class schedset:
 
 
 class schedthread:
-    
+
     def __init__(self, sched, lock=None):
         self._sched = sched
         self._lock = lock
@@ -88,8 +93,8 @@ class schedthread:
 
     def run(self):
         self._running = True
-        thread.start_new_thread(self._loop, ())
-        
+        _thread.start_new_thread(self._loop, ())
+
     def stop(self):
         self._running = False
 
@@ -110,7 +115,7 @@ class schedthread:
 
 
 class schedalarm:
-    
+
     def __init__(self, sched, lock=None):
         self._sched = sched
         self._lock = lock
@@ -123,7 +128,7 @@ class schedalarm:
         self._running = True
         signal.signal(signal.SIGALRM, self._handler)
         self._handler(None, None)
-        
+
     def stop(self):
         self._running = False
 
@@ -149,9 +154,8 @@ def _seconds_left(next):
     if not next:
         return None
     now = datetime.datetime.now()
-    delta = next-now
-    seconds = delta.days*86400+delta.seconds
+    delta = next - now
+    seconds = delta.days * 86400 + delta.seconds
     if seconds < 0:
         seconds = 0
     return seconds
-
