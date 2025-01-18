172b15ac26e1677866fa217a3e1b25ea195e5095
sylvain <szydlowskis@aol.com>
2025-01-18 15:39:12 +0100

    MonitorCategorizableTest.py ok

--- taskcoach/tests/unittests/monitorTests/MonitorCategorizableTest.py
+++ taskcoach/tests/unittests/monitorTests/MonitorCategorizableTest.py
@@ -16,25 +16,25 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import test
-from ....taskcoachlib.changes import ChangeMonitor
-from ....taskcoachlib.domain.categorizable import CategorizableCompositeObject
-from ....taskcoachlib.domain.category import Category
-from ....taskcoachlib.patterns import ObservableList
+from ... import tctest
+from taskcoachlib.changes import ChangeMonitor
+from taskcoachlib.domain.categorizable import CategorizableCompositeObject
+from taskcoachlib.domain.category import Category
+from taskcoachlib.patterns import ObservableList
 
 
-class MonitorCategorizableTest(test.TestCase):
+class MonitorCategorizableTest(tctest.TestCase):
     def setUp(self):
         self.monitor = ChangeMonitor()
         self.monitor.monitorClass(CategorizableCompositeObject)
 
-        self.obj = CategorizableCompositeObject(subject=u'Object')
+        self.obj = CategorizableCompositeObject(subject="Object")
         self.list = ObservableList()
         self.monitor.monitorCollection(self.list)
         self.list.append(self.obj)
 
-        self.cat1 = Category(subject=u'Cat #1')
-        self.cat2 = Category(subject=u'Cat #2')
+        self.cat1 = Category(subject="Cat #1")
+        self.cat2 = Category(subject="Cat #2")
         self.catList = ObservableList()
         self.catList.append(self.cat1)
         self.catList.append(self.cat2)
@@ -47,18 +47,22 @@ class MonitorCategorizableTest(test.TestCase):
         self.monitor.resetAllChanges()
         self.obj.addCategory(self.cat1)
         # self.assertEqual(self.monitor.getChanges(self.obj), set(['__add_category:%s' % self.cat1.id()]))
-        self.assertEqual(self.monitor.getChanges(self.obj), {'__add_category:%s' % self.cat1.id()})
+        self.assertEqual(
+            self.monitor.getChanges(self.obj), {f"__add_category:{self.cat1.id()}"}
+        )
 
     def testRemoveCategory(self):
         self.obj.addCategory(self.cat1)
         self.monitor.resetAllChanges()
         self.obj.removeCategory(self.cat1)
         # self.assertEqual(self.monitor.getChanges(self.obj), set(['__del_category:%s' % self.cat1.id()]))
-        self.assertEqual(self.monitor.getChanges(self.obj), {'__del_category:%s' % self.cat1.id()})
+        self.assertEqual(
+            self.monitor.getChanges(self.obj), {f"__del_category:{self.cat1.id()}"}
+        )
 
     def testRemoveBadCategory(self):
         self.obj.addCategory(self.cat1)
         self.monitor.resetAllChanges()
         self.obj.removeCategory(self.cat2)
         # self.assertEqual(self.monitor.getChanges(self.obj), set())
-        self.assertEqual(self.monitor.getChanges(self.obj), {})
+        self.assertEqual(self.monitor.getChanges(self.obj), set())
