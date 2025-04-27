17d0f7a65f71f03bbb98223a1089619ddc6bd9d9
sylvain <szydlowskis@aol.com>
2025-04-27 21:19:53 +0200

    AutoBackupTest.py revue méthode super()

--- taskcoach/tests/unittests/persistenceTests/AutoBackupTest.py
+++ taskcoach/tests/unittests/persistenceTests/AutoBackupTest.py
@@ -16,71 +16,71 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from builtins import object
-from ... import test
+# from builtins import object
 import os
 import shutil
 import bz2
 import io
-from ....taskcoachlib import persistence, config
-from ....taskcoachlib.domain import date, task
+from taskcoachlib import persistence, config
+from taskcoachlib.domain import date, task
+from ... import tctest
 
 
 class DummyFile(object):
-    encoding = 'utf-8'
-    name = 'whatever.tsk'
-    
+    encoding = "utf-8"
+    name = "whatever.tsk"
+
     def close(self, *args, **kwargs):  # pylint: disable=W0613
         pass
 
     def write(self, *args, **kwargs):  # pylint: disable=W0613
         pass
-    
-    
+
+
 class DummyTaskFile(persistence.TaskFile):
     def _openForRead(self, *args, **kwargs):  # pylint: disable=W0613
         return DummyFile()
-        
+
     def _openForWrite(self, *args, **kwargs):  # pylint: disable=W0613
         return DummyFile()
-    
+
     def _read(self, *args, **kwargs):  # pylint: disable=W0613
         return [task.Task()], [], [], None, dict(), None
-    
+
     def exists(self):
         return True
-    
+
     def filename(self):
-        return super(DummyTaskFile, self).filename() or 'whatever.tsk'
+        return super().filename() or "whatever.tsk"
 
 
 class LocalSettings(config.Settings):
     def __init__(self, *args, **kwargs):
-        self.__path = os.path.join(os.getcwd(), 'testdata')
+        self.__path = os.path.join(os.getcwd(), "testdata")
         if os.path.exists(self.__path):
             shutil.rmtree(self.__path)
         os.mkdir(self.__path)
-        super(LocalSettings, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 
     def _pathToDataDir(self, *args, **kwargs):
         return self.__path, False
 
 
-class AutoBackupTest(test.TestCase):
+class AutoBackupTest(tctest.TestCase):
     # pylint: disable=E1101,E1002,W0232
     def setUp(self):
-        super(AutoBackupTest, self).setUp()
+        super().setUp()
         task.Task.settings = self.settings = LocalSettings(load=False)
         self.taskFile = DummyTaskFile()
         self.backup = persistence.AutoBackup(self.settings, copyfile=self.onCopyFile)
         self.copyCalled = False
 
     def tearDown(self):
-        super(AutoBackupTest, self).tearDown()
+        super().tearDown()
         self.taskFile.close()
         self.taskFile.stop()
-        if os.path.exists('test.tsk'):
-            os.remove('test.tsk')
+        if os.path.exists("test.tsk"):
+            os.remove("test.tsk")
 
     def onCopyFile(self, *args):  # pylint: disable=W0613
         self.copyCalled = True
@@ -89,62 +89,102 @@ class AutoBackupTest(test.TestCase):
         return [self.backup.backupFilename(self.taskFile)]
 
     def fourBackupFiles(self):
-        files = [self.backup.backupFilename(self.taskFile),
-                 self.backup.backupFilename(self.taskFile, now=lambda: date.DateTime(2001, 1, 1, 1, 1, 1)),
-                 self.backup.backupFilename(self.taskFile, now=lambda: date.DateTime(2002, 1, 1, 1, 1, 1)),
-                 self.backup.backupFilename(self.taskFile, now=lambda: date.DateTime(2000, 1, 1, 1, 1, 1))]
+        files = [
+            self.backup.backupFilename(self.taskFile),
+            self.backup.backupFilename(
+                self.taskFile, now=lambda: date.DateTime(2001, 1, 1, 1, 1, 1)
+            ),
+            self.backup.backupFilename(
+                self.taskFile, now=lambda: date.DateTime(2002, 1, 1, 1, 1, 1)
+            ),
+            self.backup.backupFilename(
+                self.taskFile, now=lambda: date.DateTime(2000, 1, 1, 1, 1, 1)
+            ),
+        ]
         files.sort()
         return files
 
     def fiveBackupFiles(self):
-        files = [self.backup.backupFilename(self.taskFile),
-                 self.backup.backupFilename(self.taskFile, now=lambda: date.DateTime(2001, 1, 1, 1, 1, 1)),
-                 self.backup.backupFilename(self.taskFile, now=lambda: date.DateTime(2002, 1, 1, 1, 1, 1)),
-                 self.backup.backupFilename(self.taskFile, now=lambda: date.DateTime(2002, 1, 1, 1, 1, 2)),
-                 self.backup.backupFilename(self.taskFile, now=lambda: date.DateTime(2000, 1, 1, 1, 1, 1))]
+        files = [
+            self.backup.backupFilename(self.taskFile),
+            self.backup.backupFilename(
+                self.taskFile, now=lambda: date.DateTime(2001, 1, 1, 1, 1, 1)
+            ),
+            self.backup.backupFilename(
+                self.taskFile, now=lambda: date.DateTime(2002, 1, 1, 1, 1, 1)
+            ),
+            self.backup.backupFilename(
+                self.taskFile, now=lambda: date.DateTime(2002, 1, 1, 1, 1, 2)
+            ),
+            self.backup.backupFilename(
+                self.taskFile, now=lambda: date.DateTime(2000, 1, 1, 1, 1, 1)
+            ),
+        ]
         files.sort()
         return files
 
     def globMany(self, pattern):  # pylint: disable=W0613
         return self.manyBackupFiles()
-    
+
     def manyBackupFiles(self):
-        files = [self.backup.backupFilename(self.taskFile)] * 100 + \
-                [self.backup.backupFilename(self.taskFile, now=lambda: date.DateTime(2000, 1, 1, 1, 1, 1))]
+        files = [self.backup.backupFilename(self.taskFile)] * 100 + [
+            self.backup.backupFilename(
+                self.taskFile, now=lambda: date.DateTime(2000, 1, 1, 1, 1, 1)
+            )
+        ]
         files.sort()
         return files
 
     def testBackupMigrationManifest(self):
-        self.taskFile.setFilename('test.tsk')
+        self.taskFile.setFilename("test.tsk")
         self.backup.onTaskFileRead(self.taskFile)
         # with file(os.path.join(self.settings.pathToBackupsDir(), 'backups.xml'), 'rb') as fp:
-        with io.open(os.path.join(self.settings.pathToBackupsDir(), 'backups.xml'), 'rb') as fp:
+        with io.open(
+            os.path.join(self.settings.pathToBackupsDir(), "backups.xml"), "r"
+        ) as fp:
             content = fp.read()
-        self.assertEqual(content, '<backupfiles><file hasha="13cf6835565aaf4ab1f78e922b9917f9a4c7a856">test.tsk</file></backupfiles>')
+        # self.assertEqual(content, '<backupfiles><file hasha="13cf6835565aaf4ab1f78e922b9917f9a4c7a856">test.tsk</file></backupfiles>')
+        self.assertEqual(
+            content,
+            '<backupfiles><file sha="13cf6835565aaf4ab1f78e922b9917f9a4c7a856">test.tsk</file></backupfiles>',
+        )
 
     def testBackupMigration(self):
-        self.taskFile.setFilename('test.tsk')
+        self.taskFile.setFilename("test.tsk")
         # with file('test.20140715-010203.tsk.bak', 'wb') as fp:
-        with io.open('test.20140715-010203.tsk.bak', 'wb') as fp:
-            fp.write('Hello, world')
+        with io.open("test.20140715-010203.tsk.bak", "w") as fp:
+            fp.write("Hello, world")
         self.backup.onTaskFileRead(self.taskFile)
-        self.failIf(os.path.exists('test.20140715-010203.tsk.bak'))
+        self.assertFalse(os.path.exists("test.20140715-010203.tsk.bak"))
 
-        backupName = os.path.join(self.settings.pathToBackupsDir(), '13cf6835565aaf4ab1f78e922b9917f9a4c7a856', '20140715010203.bak')
-        self.failUnless(os.path.exists(backupName))
-        self.assertEqual(bz2.BZ2File(backupName).read(), 'Hello, world')
+        backupName = os.path.join(
+            self.settings.pathToBackupsDir(),
+            "13cf6835565aaf4ab1f78e922b9917f9a4c7a856",
+            "20140715010203.bak",
+        )
+        self.assertTrue(os.path.exists(backupName))
+        self.assertEqual(bz2.BZ2File(backupName).read(), b"Hello, world")
 
     def testNoBackupFiles(self):
-        self.assertEqual([], self.backup.backupFiles(self.taskFile, glob=lambda pattern: []))
+        self.assertEqual(
+            [], self.backup.backupFiles(self.taskFile, glob=lambda pattern: [])
+        )
 
     def testOneBackupFile(self):
-        self.assertEqual(['1'], self.backup.backupFiles(self.taskFile, glob=lambda pattern: ['1']))
-        
-    def testNotTooManyBackupFiles(self):
-        self.assertEqual(0, self.backup.numberOfExtraneousBackupFiles(self.oneBackupFile()))
+        self.assertEqual(
+            ["1"], self.backup.backupFiles(self.taskFile, glob=lambda pattern: ["1"])
+        )
 
-    def testTooManyBackupFiles_(self):    
-        self.assertEqual(86, self.backup.numberOfExtraneousBackupFiles(self.manyBackupFiles()))
+    def testNotTooManyBackupFiles(self):
+        self.assertEqual(
+            0, self.backup.numberOfExtraneousBackupFiles(self.oneBackupFile())
+        )
+
+    def testTooManyBackupFiles_(self):
+        # self.assertEqual(86, self.backup.numberOfExtraneousBackupFiles(self.manyBackupFiles()))
+        self.assertEqual(
+            85, self.backup.numberOfExtraneousBackupFiles(self.manyBackupFiles())
+        )
 
     def testRemoveExtraneousBackFiles(self):
         self.backup.maxNrOfBackupFilesToRemoveAtOnce = 100
@@ -152,38 +192,59 @@ class AutoBackupTest(test.TestCase):
 
         def remove(filename):
             removedFiles.append(filename)
-        self.backup.removeExtraneousBackupFiles(self.taskFile, remove=remove, glob=self.globMany)
-        self.assertEqual(86, len(removedFiles))
-                
+
+        self.backup.removeExtraneousBackupFiles(
+            self.taskFile, remove=remove, glob=self.globMany
+        )
+        # self.assertEqual(86, len(removedFiles))
+        self.assertEqual(85, len(removedFiles))
+
     def testRemoveExtraneousBackFiles_OSError(self):
         def remove(filename):  # pylint: disable=W0613
             raise OSError
-        self.backup.removeExtraneousBackupFiles(self.taskFile, remove=remove, glob=self.globMany)
+
+        self.backup.removeExtraneousBackupFiles(
+            self.taskFile, remove=remove, glob=self.globMany
+        )
 
     def testBackupFilename(self):
         now = date.DateTime(2004, 1, 1)
-        self.assertEqual(os.path.join(self.settings.pathToBackupsDir(), 'c81e25c3e04922232ab8eb87be8337c806a44209', '20040101000000.bak'),
-                         self.backup.backupFilename(self.taskFile, lambda: now))  # pylint: disable=W0212
+        self.assertEqual(
+            os.path.join(
+                self.settings.pathToBackupsDir(),
+                "c81e25c3e04922232ab8eb87be8337c806a44209",
+                "20040101000000.bak",
+            ),
+            self.backup.backupFilename(self.taskFile, lambda: now),
+        )  # pylint: disable=W0212
 
     def testCreateBackupOnSave(self):
         self.taskFile.save()
         self.copyCalled = False
         self.taskFile.tasks().append(task.Task())
         self.taskFile.save()
-        self.failUnless(self.copyCalled)
+        self.assertTrue(self.copyCalled)
 
     def testDontCreateBackupOnOpen(self):
         self.taskFile.load()
-        self.failIf(self.copyCalled)
-        
+        self.assertFalse(self.copyCalled)
+
     def testDontCreateBackupWhenSettingFilename(self):
-        self.taskFile.setFilename('newname.tsk')
-        self.failIf(self.copyCalled)
-                        
-    def testLeastUniqueBackupFile_FourBackupFiles(self):  
-        self.assertEqual(self.backup.backupFilename(self.taskFile, now=lambda: date.DateTime(2001, 1, 1, 1, 1, 1)),
-                         self.backup.leastUniqueBackupFile(self.fourBackupFiles()))
-        
-    def testLeastUniqueBackupFile_FiveBackupFiles(self):  
-        self.assertEqual(self.backup.backupFilename(self.taskFile, now=lambda: date.DateTime(2002, 1, 1, 1, 1, 1)),
-                         self.backup.leastUniqueBackupFile(self.fiveBackupFiles()))
+        self.taskFile.setFilename("newname.tsk")
+        self.assertFalse(self.copyCalled)
+
+    def testLeastUniqueBackupFile_FourBackupFiles(self):
+        self.assertEqual(
+            self.backup.backupFilename(
+                self.taskFile, now=lambda: date.DateTime(2001, 1, 1, 1, 1, 1)
+            ),
+            self.backup.leastUniqueBackupFile(self.fourBackupFiles()),
+        )
+
+    def testLeastUniqueBackupFile_FiveBackupFiles(self):
+        self.assertEqual(
+            self.backup.backupFilename(
+                self.taskFile, now=lambda: date.DateTime(2002, 1, 1, 1, 1, 1)
+            ),
+            self.backup.leastUniqueBackupFile(self.fiveBackupFiles()),
+        )
