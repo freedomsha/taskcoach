95032f2380bce01a641eb325e3dc4df5d332171e
sylvain <szydlowskis@aol.com>
2025-04-28 13:15:21 +0200

    SyncMLTest.py

--- taskcoach/tests/unittests/persistenceTests/SyncMLTest.py
+++ taskcoach/tests/unittests/persistenceTests/SyncMLTest.py
@@ -16,45 +16,46 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from ... import test
-from ....taskcoachlib import persistence
-from ....taskcoachlib.domain import task, note, category, effort, date
+from ... import tctest
+from taskcoachlib import persistence
+from taskcoachlib.domain import task, note, category, effort, date
 
 
-class TaskFileTestCase(test.TestCase):
+class TaskFileTestCase(tctest.TestCase):
     def setUp(self):
         self.taskFile = persistence.TaskFile()
-        self.task = task.Task(subject='Subject')
+        self.task = task.Task(subject="Subject")
 
     def tearDown(self):
-        super(TaskFileTestCase, self).tearDown()
+        # super(TaskFileTestCase, self).tearDown()
+        super().tearDown()
         self.taskFile.close()
         self.taskFile.stop()
 
     def testTaskIsDirtyAfterEditingSubject(self):
         self.taskFile.tasks().append(self.task)
-        self.failUnless(task.Task.STATUS_NEW, self.task.getStatus())
-        self.task.setSubject('New subject')
-        self.failUnless(task.Task.STATUS_CHANGED, self.task.getStatus())
+        self.assertTrue(task.Task.STATUS_NEW, self.task.getStatus())
+        self.task.setSubject("New subject")
+        self.assertTrue(task.Task.STATUS_CHANGED, self.task.getStatus())
 
     def testNoteIsDirtyAfterEditingSubject(self):
-        aNote = note.Note(subject='Subject')
+        aNote = note.Note(subject="Subject")
         self.taskFile.notes().append(aNote)
-        self.failUnless(note.Note.STATUS_NEW, aNote.getStatus())
-        aNote.setSubject('New subject')
-        self.failUnless(note.Note.STATUS_CHANGED, aNote.getStatus())
+        self.assertTrue(note.Note.STATUS_NEW, aNote.getStatus())
+        aNote.setSubject("New subject")
+        self.assertTrue(note.Note.STATUS_CHANGED, aNote.getStatus())
 
     def testCategoryIsDirtyAfterEditingSubject(self):
-        aCategory = category.Category(subject='Subject')
+        aCategory = category.Category(subject="Subject")
         self.taskFile.categories().append(aCategory)
-        self.failUnless(category.Category.STATUS_NEW, aCategory.getStatus())
-        aCategory.setSubject('New subject')
-        self.failUnless(category.Category.STATUS_CHANGED, aCategory.getStatus())
+        self.assertTrue(category.Category.STATUS_NEW, aCategory.getStatus())
+        aCategory.setSubject("New subject")
+        self.assertTrue(category.Category.STATUS_CHANGED, aCategory.getStatus())
 
     def testEffortIsDirtyAfterEditingStart(self):
         self.taskFile.tasks().append(self.task)
         anEffort = effort.Effort(self.task)
         self.task.addEffort(anEffort)
-        self.failUnless(effort.Effort.STATUS_NEW, anEffort.getStatus())
+        self.assertTrue(effort.Effort.STATUS_NEW, anEffort.getStatus())
         anEffort.setStart(date.DateTime(2000, 1, 1, 10, 0, 0))
-        self.failUnless(effort.Effort.STATUS_CHANGED, anEffort.getStatus())
+        self.assertTrue(effort.Effort.STATUS_CHANGED, anEffort.getStatus())
