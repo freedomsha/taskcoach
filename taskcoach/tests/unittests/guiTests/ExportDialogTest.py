93c583da09360787733ff68c568e652c0bf2a934
sylvain <szydlowskis@aol.com>
2025-04-28 10:31:58 +0200

    ExportDialogTest.py Remise en forme

--- taskcoach/tests/unittests/guiTests/ExportDialogTest.py
+++ taskcoach/tests/unittests/guiTests/ExportDialogTest.py
@@ -16,11 +16,11 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from builtins import object
-import test
-from ....taskcoachlib.gui import dialog
-from ....taskcoachlib import config
+# from builtins import object
 from wx.lib import sized_controls
+from ... import tctest
+from taskcoachlib import config
+from taskcoachlib.gui import dialog
 
 
 class DummyColumn(object):
@@ -31,23 +31,24 @@ class DummyColumn(object):
         return self.__name
 
     def header(self):
-        return 'dummy column "%s"' % self.__name
+        # return 'dummy column "%s"' % self.__name
+        return f'dummy column "{self.__name}"'
 
 
 class DummyViewer(object):
     def __init__(self):
-        self.col1 = DummyColumn('one')
-        self.col2 = DummyColumn('two')
+        self.col1 = DummyColumn("one")
+        self.col2 = DummyColumn("two")
 
     def title(self):
-        return 'viewer'
-    
+        return "viewer"
+
     def hasHideableColumns(self):
         return True
-    
+
     def visibleColumns(self):
         return []
-    
+
     def columns(self):
         return [self.col1, self.col2]
 
@@ -58,7 +59,7 @@ class DummyViewer(object):
 class DummyViewerContainer(object):
     def activeViewer(self):
         return 1
-    
+
     def __getitem__(self, index):
         if index == 0:
             return DummyViewer()
@@ -66,20 +67,20 @@ class DummyViewerContainer(object):
             raise IndexError
 
 
-class ExportDialogTest(test.wxTestCase):
+class ExportDialogTest(tctest.wxTestCase):
     def testCreate(self):
         self.frame.viewer = DummyViewerContainer()
         settings = config.Settings(load=False)
         dialog.export.ExportAsHTMLDialog(self.frame, settings=settings)
-        
-        
-class ColumnPickerTest(test.wxTestCase):
+
+
+class ColumnPickerTest(tctest.wxTestCase):
     def testCreate(self):
         panel = sized_controls.SizedPanel(self.frame)
         dialog.export.ColumnPicker(panel, DummyViewer())
-        
+
     def testOnlySelectableColumns(self):
         panel = sized_controls.SizedPanel(self.frame)
         dlg = dialog.export.ColumnPicker(panel, DummyViewer())
         self.assertEqual(dlg.columnPicker.GetCount(), 1)
-        self.assertEqual(dlg.columnPicker.GetClientData(0).name(), 'two')
+        self.assertEqual(dlg.columnPicker.GetClientData(0).name(), "two")
