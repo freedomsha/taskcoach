d9191138bb878ae51363063ff68f407752b11fe4
sylvain <szydlowskis@aol.com>
2025-04-28 09:16:25 +0200

    DateTimeEntryTest.py mise en forme

--- taskcoach/tests/unittests/guiTests/DateTimeEntryTest.py
+++ taskcoach/tests/unittests/guiTests/DateTimeEntryTest.py
@@ -16,17 +16,18 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import test
-from ....taskcoachlib.gui.dialog import entry
-from ....taskcoachlib.domain import date
-from ....taskcoachlib import config
+from ... import tctest
+from taskcoachlib.gui.dialog import entry
+from taskcoachlib.domain import date
+from taskcoachlib import config
 
 
-class DateTimeEntryTest(test.wxTestCase):
+class DateTimeEntryTest(tctest.wxTestCase):
     def setUp(self):
-        super(DateTimeEntryTest, self).setUp()
-        self.dateTimeEntry = entry.DateTimeEntry(self.frame,
-                                                 config.Settings(load=False))
+        super().setUp()
+        self.dateTimeEntry = entry.DateTimeEntry(
+            self.frame, config.Settings(load=False)
+        )
         self.dateTime = date.DateTime(2004, 1, 1)
 
     def testCreate(self):
@@ -35,8 +36,9 @@ class DateTimeEntryTest(test.wxTestCase):
     def testSet(self):
         now = date.Now()
         self.dateTimeEntry.SetValue(now)
-        self.assertAlmostEqual(now.toordinal(),
-                               self.dateTimeEntry.GetValue().toordinal(), places=2)
+        self.assertAlmostEqual(
+            now.toordinal(), self.dateTimeEntry.GetValue().toordinal(), places=2
+        )
 
     def testReset(self):
         self.dateTimeEntry.SetValue()
@@ -47,51 +49,46 @@ class DateTimeEntryTest(test.wxTestCase):
         self.assertEqual(self.dateTime, self.dateTimeEntry.GetValue())
 
 
-class DateEntryConstructorTest(test.wxTestCase):
+class DateEntryConstructorTest(tctest.wxTestCase):
     def testCreateWithDate(self):
         tomorrow = date.Tomorrow()
-        dateTimeEntry = entry.DateTimeEntry(self.frame,
-                                            config.Settings(load=False),
-                                            tomorrow)
-        self.assertAlmostEqual(tomorrow.toordinal(),
-                               dateTimeEntry.GetValue().toordinal(),
-                               places=2)
+        dateTimeEntry = entry.DateTimeEntry(
+            self.frame, config.Settings(load=False), tomorrow
+        )
+        self.assertAlmostEqual(
+            tomorrow.toordinal(), dateTimeEntry.GetValue().toordinal(), places=2
+        )
 
 
-class TimeDeltaEntryTest(test.wxTestCase):
+class TimeDeltaEntryTest(tctest.wxTestCase):
     def setUp(self):
-        super(TimeDeltaEntryTest, self).setUp()
+        super().setUp()
         self.timeDeltaEntry = entry.TimeDeltaEntry(self.frame)
 
     def testDefaultValue(self):
         self.assertEqual(date.TimeDelta(), self.timeDeltaEntry.GetValue())
 
     def testDefaultDisplayedValue(self):
-        self.assertEqual('        0:00:00',
-                         self.timeDeltaEntry._entry.GetValue())
+        self.assertEqual("        0:00:00", self.timeDeltaEntry._entry.GetValue())
 
     def testSetValue(self):
         self.timeDeltaEntry.SetValue(date.TimeDelta(hours=10, seconds=5))
-        self.assertEqual('       10:00:05',
-                         self.timeDeltaEntry._entry.GetValue())
+        self.assertEqual("       10:00:05", self.timeDeltaEntry._entry.GetValue())
 
     def testOverflow(self):
         self.timeDeltaEntry.SetValue(date.TimeDelta(hours=12345678912))
-        self.assertEqual('123456789:00:00',
-                         self.timeDeltaEntry._entry.GetValue())
+        self.assertEqual("123456789:00:00", self.timeDeltaEntry._entry.GetValue())
 
 
-class ReadOnlyTimeDeltaEntryTest(test.wxTestCase):
+class ReadOnlyTimeDeltaEntryTest(tctest.wxTestCase):
     def setUp(self):
-        super(ReadOnlyTimeDeltaEntryTest, self).setUp()
+        super().setUp()
         self.timeDeltaEntry = entry.TimeDeltaEntry(self.frame, readonly=True)
 
     def testSetNegativeValue(self):
         self.timeDeltaEntry.SetValue(date.TimeDelta(hours=-10, minutes=-20))
-        self.assertEqual('      -10:20:00',
-                         self.timeDeltaEntry._entry.GetValue())
+        self.assertEqual("      -10:20:00", self.timeDeltaEntry._entry.GetValue())
 
     def testSetSmallNegativeValue(self):
         self.timeDeltaEntry.SetValue(date.TimeDelta(seconds=-4))
-        self.assertEqual('       -0:00:04',
-                         self.timeDeltaEntry._entry.GetValue())
+        self.assertEqual("       -0:00:04", self.timeDeltaEntry._entry.GetValue())
