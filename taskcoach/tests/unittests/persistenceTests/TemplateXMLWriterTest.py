fa567df17edb5b9138f632b4cab03b5175d1621f
sylvain <szydlowskis@aol.com>
2025-04-28 13:26:00 +0200

    TemplatesXMLWriterTest.py Méthode super() self.fd = io.StringIO() ou io.BytesIO() ? encoding or not ?

--- taskcoach/tests/unittests/persistenceTests/TemplateXMLWriterTest.py
+++ taskcoach/tests/unittests/persistenceTests/TemplateXMLWriterTest.py
@@ -16,50 +16,54 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from future import standard_library
-from ... import test
+# from future import standard_library
+from ... import tctest
 import io
-from ....taskcoachlib import persistence, config
-from ....taskcoachlib.domain import task, date
-standard_library.install_aliases()
+from taskcoachlib import persistence, config
+from taskcoachlib.domain import task, date
+
+# standard_library.install_aliases()
 
 
-class TemplateXMLWriterTestCase(test.TestCase):
+class TemplateXMLWriterTestCase(tctest.TestCase):
     def setUp(self):
         task.Task.settings = config.Settings(load=False)
-        self.fd = io.StringIO()
-        self.fd.name = 'testfile.tsk'
-        self.fd.encoding = 'utf-8'
+        # self.fd = io.StringIO()  # This creates a StringIO object
+        self.fd = io.BytesIO()  # This creates a BytesIO object TODO : Choisir !
+        # self.fd.name = 'testfile.tsk'  # Name attribute assignment might not be necessary for StringIO
+        # self.fd.encoding = 'utf-8'  # Remove or comment this out if it's present in your code
         self.writer = persistence.TemplateXMLWriter(self.fd)
         self.task = task.Task()
-        
+
     def __writeAndRead(self):
-        self.writer.write(self.task)
+        self.writer.write(self.task)  # TODO:Manque paramètres categoryContainer: {rootItems}, noteContainer: {rootItems},
+        # syncMLConfig: Any, guid: str
         return self.fd.getvalue()
-    
+
     def expectInXML(self, xmlFragment):
         xml = self.__writeAndRead()
-        self.failUnless(xmlFragment in xml, '%s not in %s' % (xmlFragment, xml))
-        
+        self.assertTrue(xmlFragment in xml, "%s not in %s" % (xmlFragment, xml))
+
     # tests
-    
+
     def testDefaultTask(self):
-        self.expectInXML('<tasks>\n<task creationDateTime="%s" id="%s" '
-                         'status="1" />\n</tasks>' % (self.task.creationDateTime(),
-                                                      self.task.id()))
-        
+        self.expectInXML(
+            '<tasks>\n<task creationDateTime="%s" id="%s" '
+            'status="1" />\n</tasks>' % (self.task.creationDateTime(), self.task.id())
+        )
+
     def testTaskWithPlannedStartDateTime(self):
         self.task.setPlannedStartDateTime(date.Now() + date.TimeDelta(minutes=31))
         self.expectInXML('plannedstartdatetmpl="31 minutes from Now')
-        
+
     def testTaskWithDueDateTime(self):
         self.task.setDueDateTime(date.Now() + date.TimeDelta(minutes=13))
         self.expectInXML('duedatetmpl="13 minutes from Now')
-        
+
     def testTaskWithCompletionDateTime(self):
         self.task.setCompletionDateTime(date.Now() + date.TimeDelta(minutes=4))
         self.expectInXML('completiondatetmpl="4 minutes from Now')
-        
+
     def testTaskWithReminder(self):
         self.task.setReminder(date.Now() + date.TimeDelta(seconds=10))
         self.expectInXML('remindertmpl="0 minutes from Now')
