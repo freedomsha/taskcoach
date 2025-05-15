e8de4a5d78aa1f594886f93cad3b94378dd6147c
sylvain <szydlowskis@aol.com>
2025-05-15 10:04:15 +0200

    lockfile in thirdparty !

--- taskcoach/tests/unittests/thirdPartySoftwareTests/test_lockfile.py
+++ taskcoach/tests/unittests/thirdPartySoftwareTests/test_lockfile.py
@@ -16,8 +16,9 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import lockfile
+# import lockfile
 import tempfile
+from taskcoachlib.thirdparty import lockfile
 from ... import tctest
 
 
