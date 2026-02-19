f60e0115c41d3d89dd84855374f8319f8fa482a8
sylvain <szydlowskis@aol.com>
2026-02-19 19:08:33 +0100

    Refonte de l'interface de BackupManager avec wx.SplitterWindow et ajustements de redimensionnement
    - Utilisation de wx.SplitterWindow pour séparer les listes de fichiers et de sauvegardes.
    - Ajout d'un panneau latéral pour les boutons et d'un bouton "Fermer".
    - Dimensionnement du dialogue contraint par la taille de l'écran.
    - Amélioration du calcul de la largeur des colonnes pour la liste des sauvegardes.

--- taskcoach/taskcoachlib/gui/dialog/backupmanager.py
+++ taskcoach/taskcoachlib/gui/dialog/backupmanager.py
@@ -27,18 +27,58 @@ class BackupManagerDialog(wx.Dialog):
     """
     Dialogue pour gérer les sauvegardes de fichiers.
     """
+
     def __init__(self, parent, settings, selectedFile=None):
-        super().__init__(parent, wx.ID_ANY, style=wx.DEFAULT_DIALOG_STYLE | wx.RESIZE_BORDER)
+        super().__init__(
+            parent, wx.ID_ANY, style=wx.DEFAULT_DIALOG_STYLE | wx.RESIZE_BORDER
+        )
 
         container = wx.Panel(self)
-        self.__files = wx.ListCtrl(container, wx.ID_ANY, style=wx.LC_REPORT | wx.LC_SINGLE_SEL)
+        # Create splitter for the two lists
+        self.__splitter = wx.SplitterWindow(
+            self, wx.ID_ANY, style=wx.SP_LIVE_UPDATE
+        )
+        # Left pane: Files list
+        self.__files = wx.ListCtrl(
+            container, wx.ID_ANY, style=wx.LC_REPORT | wx.LC_SINGLE_SEL
+        )
+        # self.__files = wx.ListCtrl(
+        #     self.__splitter, wx.ID_ANY, style=wx.LC_REPORT | wx.LC_SINGLE_SEL
+        # )
         self.__files.InsertColumn(0, _("File"))
         self.__files.InsertColumn(1, _("Full path"))
-        self.__backups = wx.ListCtrl(container, wx.ID_ANY, style=wx.LC_REPORT | wx.LC_SINGLE_SEL)
+        # Right pane: Backups/Date list
+        self.__backups = wx.ListCtrl(
+            container, wx.ID_ANY, style=wx.LC_REPORT | wx.LC_SINGLE_SEL
+        )
+        # self.__backups = wx.ListCtrl(
+        #     self.__splitter, wx.ID_ANY, style=wx.LC_REPORT | wx.LC_SINGLE_SEL
+        # )
         self.__backups.InsertColumn(0, _("Date"))
         self.__backups.Enable(False)
+
+        # Configure splitter
+        self.__splitter.SplitVertically(self.__files, self.__backups)
+        self.__splitter.SetMinimumPaneSize(150)
+        self.__splitter.SetSashGravity(1.0)
+
+        # Button column
+        btnPanel = wx.Panel(self)
+        btnSizer = wx.BoxSizer(wx.VERTICAL)
         self.__btnRestore = wx.Button(container, wx.ID_ANY, _("Restore"))
         self.__btnRestore.Enable(False)
+        btnClose = wx.Button(btnPanel, wx.ID_ANY, _("Close"))
+        btnSizer.Add(self.__btnRestore, 0, wx.ALL, 3)
+        btnSizer.AddStretchSpacer(1)
+        btnSizer.Add(btnClose, 0, wx.ALL, 3)
+        btnPanel.SetSizer(btnSizer)
+
+        # Main horizontal layout: splitter + buttons
+        mainSizer = wx.BoxSizer(wx.HORIZONTAL)
+        mainSizer.Add(self.__splitter, 1, wx.EXPAND | wx.ALL, 3)
+        mainSizer.Add(btnPanel, 0, wx.EXPAND | wx.ALL, 3)
+        self.SetSizer(mainSizer)
+
         self.__filename = selectedFile
         self.__selection = (None, None)
         # TODO: Try:
@@ -64,7 +104,9 @@ class BackupManagerDialog(wx.Dialog):
         selection = None
         for filename in self.__filenames:
             # item = self.__files.InsertStringItem(self.__files.GetItemCount(), os.path.split(filename)[-1])
-            item = self.__files.InsertItem(self.__files.GetItemCount(), os.path.split(filename)[-1])
+            item = self.__files.InsertItem(
+                self.__files.GetItemCount(), os.path.split(filename)[-1]
+            )
             # self.__files.SetStringItem(item, 1, filename)
             self.__files.SetItem(item, 1, filename)
             if filename == selectedFile:
@@ -91,10 +133,26 @@ class BackupManagerDialog(wx.Dialog):
         self.__btnRestore.Bind(wx.EVT_BUTTON, self._OnRestore)
 
         if selection is not None:
-            self.__files.SetItemState(selection, wx.LIST_STATE_FOCUSED | wx.LIST_STATE_SELECTED,
-                                      wx.LIST_STATE_FOCUSED | wx.LIST_STATE_SELECTED)
+            self.__files.SetItemState(
+                selection,
+                wx.LIST_STATE_FOCUSED | wx.LIST_STATE_SELECTED,
+                wx.LIST_STATE_FOCUSED | wx.LIST_STATE_SELECTED,
+            )
         self.__files.SetColumnWidth(0, -1)
         self.__files.SetColumnWidth(1, -1)
+
+        # Set dialog size constrained to screen
+        targetWidth, targetHeight = 800, 600
+        datePaneWidth = 150
+        display = wx.Display(wx.Display.GetFromWindow(parent) if parent else 0)
+        screenRect = display.GetClientArea()
+        width = min(targetWidth, screenRect.GetWidth() - 50)
+        height = min(targetHeight, screenRect.GetHeight() - 50)
+        self.SetSize(wx.Size(width, height))
+
+        # Set sash position from right edge (negative value)
+        self.__splitter.SetSashPosition(-datePaneWidth)
+        self.CentreOnParent()
 
     def restoredFilename(self):
         return self.__filename
@@ -104,10 +162,21 @@ class BackupManagerDialog(wx.Dialog):
 
     def _OnSelectFile(self, event):
         self.__backups.DeleteAllItems()
-        for index, dateTime in enumerate(self.__manifest.listBackups(self.__filenames[event.GetIndex()])):
+        for index, dateTime in enumerate(
+            self.__manifest.listBackups(self.__filenames[event.GetIndex()])
+        ):
             # self.__backups.InsertStringItem(index, render.dateTime(dateTime, humanReadable=True))
-            self.__backups.InsertItem(index, render.dateTime(dateTime, humanReadable=True))
-        self.__backups.SetColumnWidth(0, -1)
+            self.__backups.InsertItem(
+                index, render.dateTime(dateTime, humanReadable=True)
+            )
+        # self.__backups.SetColumnWidth(0, -1)
+        # Size column to max of header width and content width
+        self.__backups.SetColumnWidth(0, wx.LIST_AUTOSIZE_USEHEADER)
+        headerWidth = self.__backups.GetColumnWidth(0)
+        self.__backups.SetColumnWidth(0, wx.LIST_AUTOSIZE)
+        contentWidth = self.__backups.GetColumnWidth(0)
+        self.__backups.SetColumnWidth(0, max(headerWidth, contentWidth))
+        self.__backups.Refresh()
         self.__backups.Enable(True)
         self.__selection = (self.__filenames[event.GetIndex()], None)
 
@@ -120,7 +189,10 @@ class BackupManagerDialog(wx.Dialog):
     def _OnSelectBackup(self, event):
         self.__btnRestore.Enable(True)
         filename = self.__selection[0]
-        self.__selection = (filename, self.__manifest.listBackups(filename)[event.GetIndex()])
+        self.__selection = (
+            filename,
+            self.__manifest.listBackups(filename)[event.GetIndex()],
+        )
 
     def _OnDeselectBackup(self, event):
         self.__btnRestore.Enable(False)
@@ -130,14 +202,20 @@ class BackupManagerDialog(wx.Dialog):
         # filename: Unexpected type(s):(None)Possible type(s):(PathLike[AnyStr])(AnyOrLiteralStr)
         # voir ligne 40
         filename, dateTime = self.__selection
-        dlg = wx.FileDialog(self, _("Choose the restoration destination"),
-                            defaultDir=os.path.dirname(filename),
-                            defaultFile=os.path.split(filename)[-1],
-                            wildcard="*.tsk", style=wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT)
+        dlg = wx.FileDialog(
+            self,
+            _("Choose the restoration destination"),
+            defaultDir=os.path.dirname(filename),
+            defaultFile=os.path.split(filename)[-1],
+            wildcard="*.tsk",
+            style=wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT,
+        )
         try:
             if dlg.ShowModal() == wx.ID_OK:
                 self.__filename = dlg.GetPath()
-                self.__manifest.restoreFile(filename, dateTime, self.__filename)
+                self.__manifest.restoreFile(
+                    filename, dateTime, self.__filename
+                )
                 self.EndModal(wx.ID_OK)
         finally:
             dlg.Destroy()
